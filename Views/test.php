61fb1d5fa7c1613f7f1c49f15c9949cedeb587f7
Maroun Nawwar <68354149+MarounNawwar@users.noreply.github.com>
2022-07-27 22:16:26 +0300

    Update test.php

--- Views/test.php
+++ Views/test.php
@@ -1,19 +1,3 @@
-<?php
-    
-    import("Database");
-
-    // $conn = Database::getConnection();
-
-    // print_r($conn->get_table_rows_from_db("test_table",[]));
-
-    // $rslt = $conn->validate_query_content("INSERT INTO `test`('val1','val2') VALUES('','')");
-
-    // if($rslt === false){
-    //     echo "Bad Query Structure";
-    // }else{
-    //     echo "Correct Query Structure";
-    // }
-
-    // log_write();
+<?php    
 
 ?>
